2f0519c7dc2fc17755e7c95c0adf9f4bc5ec7d89
Sumedh <sumedh@Sumedhs-MacBook-Air.local>
2020-04-19 21:25:55 +0530

    test file added

---
+++ src/test/java/com/springboot/swaggerexample/SwaggerExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.swaggerexample;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SwaggerExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
